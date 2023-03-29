4a3a8a1cc16b62779ff799bd61aa149f82cad3bd
yahia <yahiamarzouk1995@gmail.com>
2023-03-29 15:23:30 +0200

    exo state 1

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 function App() {
   const pokemonList = [
@@ -7,11 +8,50 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
-  let pokemon = pokemonList[0];
-  return <PokemonCard poke={pokemon} />;
+  const [pokemonIndex, setPokemonInex] = useState(0);
+
+  let pokemon = pokemonList[pokemonIndex];
+
+  const hendleSuivant = () => {
+    if (pokemonIndex >= pokemonList.length - 1) {
+      alert("index superieur");
+    } else {
+      setPokemonInex(pokemonIndex + 1);
+    }
+  };
+  const hendlePrecedent = () => {
+    if (pokemonIndex <= 0) {
+      alert("index inferieur");
+    } else {
+      setPokemonInex(pokemonIndex - 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={hendleSuivant}>suivant</button>
+      <button onClick={hendlePrecedent}>precedent </button>
+      <PokemonCard poke={pokemon} />;
+    </div>
+  );
 }
 
 export default App;
