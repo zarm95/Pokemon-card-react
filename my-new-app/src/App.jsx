bd040c3165496f1131fc643f9463def480ac2d46
yahia <yahiamarzouk1995@gmail.com>
2023-03-29 14:30:39 +0200

    exo sur le props.1

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,6 +1,17 @@
 import PokemonCard from "./components/PokemonCard";
 function App() {
-  return <PokemonCard />;
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  let pokemon = pokemonList[0];
+  return <PokemonCard poke={pokemon} />;
 }
 
 export default App;
--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 
-const PokemonCard = () => {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
+const PokemonCard = ({ poke }) => {
   let displayPoke = (pokemon) => {
     return pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>...</p>;
   };
-  let pokemon = pokemonList[0];
+
   return (
     <figure>
-      {displayPoke(pokemon)}
-      <figcaption>{pokemon.name}</figcaption>;
+      {displayPoke(poke)}
+      <figcaption>{poke.name}</figcaption>;
     </figure>
   );
 };
