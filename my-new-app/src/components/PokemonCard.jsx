eb8a6f964cd7f67bdd61cb93f26d6820bc76f915
yahia <yahiamarzouk1995@gmail.com>
2023-03-29 12:23:14 +0200

    first exo

---
+++ my-new-app/src/components/PokemonCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PokemonCard = () => {
+  return (
+    <div>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="image"
+        />
+        <figcaption>bulbasaur</figcaption>
+      </figure>
+    </div>
+  );
+};
+
+export default PokemonCard;
