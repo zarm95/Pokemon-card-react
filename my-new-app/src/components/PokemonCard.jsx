f5ac2466c5fa6c21dc15d33dd958a04dace32990
yahia <yahiamarzouk1995@gmail.com>
2023-03-29 13:59:55 +0200

    exo 2

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 
 const PokemonCard = () => {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  let displayPoke = (pokemon) => {
+    return pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>...</p>;
+  };
+  let pokemon = pokemonList[0];
   return (
-    <div>
-      <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="image"
-        />
-        <figcaption>bulbasaur</figcaption>
-      </figure>
-    </div>
+    <figure>
+      {displayPoke(pokemon)}
+      <figcaption>{pokemon.name}</figcaption>;
+    </figure>
   );
 };
 
